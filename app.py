72427d3351c0b1b4fffdaec614454869e5ac35a6
LunarianNova <lunariannova@gmail.com>
2025-05-06 17:44:00 -0400

    Baby's first broken endpoint (:

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ And I have no formal instruction in python, so be nice please (:
 '''
 from fastapi import FastAPI
 from sqlite_handler import *
+from typing import List
 
 app = FastAPI()
 
@@ -15,6 +16,17 @@ async def root() -> dict:
 
 @app.get("/team/{team_id}")
 async def get_team_by_id(team_id: str) -> dict:
-    print(team_id)
     team = teamsDatabase.fetch_team_object(team_id)
     return team.get_json()
+
+@app.get("/team/{team_id}/players", response_model=List[dict])
+async def get_players_in_team(team_id: str) -> dict:
+    max_day = playersDatabase.execute_fetchone('''SELECT MAX(day) FROM players''')[0]
+    players = playersDatabase.execute_fetchall('''SELECT * FROM players WHERE team_id = ? AND day = ?''', (team_id, max_day,))
+    players = [Player(p).get_json() for p in players]
+    offset = 0
+    while players == []:
+        offset += 1
+        players = playersDatabase.execute_fetchall(f'''SELECT * FROM players WHERE team_id = ? AND day = ?''', (team_id, max_day-offset,))
+        players = [Player(p).get_json() for p in players]
+    return players
