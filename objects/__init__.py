dfafbf4213dd2402cce660dd2b34a078ba35b780
LunarianNova <lunariannova@gmail.com>
2025-05-06 13:38:47 -0400

    ah, I forgot this one

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,7 +1,7 @@
 '''
 This reminds me of nix packages
 '''
-from game import Game
-from league import League
-from player import Player
-from team import Team
+from .game import Game
+from .league import League
+from .player import Player
+from .team import Team
